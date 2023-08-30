02155371e515dda8525c71e9a5b4081e908fb3f3
Anton Borisov <borisov2011@gmail.com>
2023-08-30 12:36:10 +0300

    добавил отображения белым цветом если фон темный

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,15 @@ const ColorConverter = () => {
   }
   };
 
+  const getContrastColor = (hexColor: string) => {
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness >= 128 ? "black" : "white";
+  };
+
+
   return (
     <div className='centered-container'>
     <div className='container'>
@@ -40,7 +49,7 @@ const ColorConverter = () => {
         onChange={handleHexChange}
         style={{ borderColor: isValid ? 'initial' : 'red' }}
       />
-      <div className='message-container'>   
+      <div className='message-container' style={{ color: getContrastColor(hexColor)}}>   
         {!isValid && <p className='error' style={{ color: 'black' }}>Ошибка!</p>}
         {rgbColor && <p className='rgb'>{rgbColor}</p>}
       </div>   
